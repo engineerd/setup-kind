e08c693bed752fb4393b997f7971ad71ad983f83
Matthieu MOREL <mmorel-35@users.noreply.github.com>
2022-01-16 12:59:27 +0100

    Update core.ts

--- src/kind/core.ts
+++ src/kind/core.ts
@@ -4,6 +4,5 @@ import process from 'process';
 export const KIND_COMMAND = process.platform === 'win32' ? 'kind.exe' : 'kind';
 
 export async function executeKindCommand(args: string[]) {
-  console.log(`Executing ${KIND_COMMAND} with args ` + args.join(' '));
   await exec.exec(KIND_COMMAND, args);
 }
