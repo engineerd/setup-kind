7ece80cb2fec38b5534a869feb6446e45b767da4
Matthieu MOREL <mmorel-35@users.noreply.github.com>
2022-02-09 23:07:09 +0100

    use catch after promise instead of try catch inside the promise

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,11 @@ import { checkEnvironment } from './requirements';
 import { installTools } from './installer';
 
 async function run() {
-  try {
-    const { kind, kubectl } = await checkEnvironment();
-    const service: KindMainService = KindMainService.getInstance();
-    await installTools(kind, kubectl);
-    await service.createCluster();
-  } catch (error) {
-    core.setFailed((error as Error).message);
-  }
+  const { kind, kubectl } = await checkEnvironment();
+  await installTools(kind, kubectl);
+  await KindMainService.getInstance().createCluster();
 }
 
-run();
+run().catch((error) => {
+  core.setFailed((error as Error).message);
+});
--- src/post.ts
+++ src/post.ts
@@ -2,13 +2,11 @@ import * as core from '@actions/core';
 import { KindPostService } from './kind/post';
 
 async function run() {
-  try {
-    const service: KindPostService = KindPostService.getInstance();
-    await service.exportClusterLogs();
-    await service.deleteCluster();
-  } catch (error) {
-    core.setFailed((error as Error).message);
-  }
+  const service: KindPostService = KindPostService.getInstance();
+  await service.exportClusterLogs();
+  await service.deleteCluster();
 }
 
-run();
+run().catch((error) => {
+  core.setFailed((error as Error).message);
+});
